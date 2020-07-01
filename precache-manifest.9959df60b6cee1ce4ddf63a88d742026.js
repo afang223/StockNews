98205f5d18738ae4a5d94de6434e2dcbaf47c988
Andy Fang <andyfang486@berkeley.edu>
2020-06-30 22:01:07 -0700

    Updates

---
+++ precache-manifest.9959df60b6cee1ce4ddf63a88d742026.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f104f5590c3b4a996d310a9586a6ff9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47202a4b11b0f79537cc",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9f7db42841a9b5626282",
+    "url": "/static/js/2.d8572339.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d8572339.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47202a4b11b0f79537cc",
+    "url": "/static/js/main.aa3706af.chunk.js"
+  },
+  {
+    "revision": "e73ce77de9ce076ac775",
+    "url": "/static/js/runtime-main.60d58910.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
